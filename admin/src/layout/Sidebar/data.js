f95a4501d777985d0cc23af8be68838160b8495e
Anietie Brownson <anni.brownson@gmail.com>
2023-03-30 19:55:37 +0100

    feat: Worked on the sidebar component

---
+++ admin/src/layout/Sidebar/data.js
@@ -0,0 +1,26 @@
+import { RxDashboard, RxAvatar } from "react-icons/rx";
+import { AiOutlineHome } from "react-icons/ai"
+import { BsBriefcase, BsPeople } from "react-icons/bs"
+
+export const SidebarLinks = [
+    {
+        icon: RxDashboard,
+        title: "Dashboard"
+    },
+    {
+        icon: AiOutlineHome,
+        title: "Home"
+    },
+    {
+        icon: BsBriefcase,
+        title: "Portfolio"
+    },
+    {
+        icon: RxAvatar,
+        title: "Avatar"
+    },
+    {
+        icon: BsPeople,
+        title: "Guest"
+    },
+]
--- admin/src/layout/Sidebar/index.js
+++ admin/src/layout/Sidebar/index.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import "./styles.css"
+import {useState} from "react";
+import "./styles.css";
+import NavLogo from "../../assets/Logo.svg";
+import { SidebarLinks } from "./data";
+import { AiOutlineLogout } from "react-icons/ai"
 
 
 const Sidebar = () => {
+    const [selected, setSelected] = useState(0)
   return (
-    <>Sidebar</>
-  )
-}
+    <>
+      <div className="sidebar">
+        <div className="logo">
+          <img src={NavLogo} alt="" />
+          <span>BeesWax</span>
+        </div>
 
-export default Sidebar
+        <div className="menu">
+          {SidebarLinks.map((item, index) => {
+            return (
+              <div className={selected===index?'menuItem active' : 'menuItem'}
+              key={index}
+              onClick={()=>setSelected(index)}
+              >
+                <item.icon />
+                <span>{item.title}</span>
+              </div>
+            );
+          })}
+
+          <div className="menuItem">
+            <AiOutlineLogout />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
