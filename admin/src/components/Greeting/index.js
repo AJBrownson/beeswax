c9512ea922a627148bab35fc4ba52d2e6015952d
Anietie Brownson <anni.brownson@gmail.com>
2023-03-31 12:14:51 +0100

    feat: Add cards in home folder

--- admin/src/components/Greeting/index.js
+++ admin/src/components/Greeting/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Greeting = () => {
-    const date = new Date();
-    const hour = date.getHours();
-    let greetings;
+  const date = new Date();
+  const hour = date.getHours();
+  let greetings;
 
-    if (hour < 12) {
-        greetings = "morning"
-    } else if (hour >= 12 && hour < 17) {
-        greetings = "afternoon"
-    } else {
-        greetings = "evening"
-    }
+  if (hour < 12) {
+    greetings = "morning";
+  } else if (hour >= 12 && hour < 17) {
+    greetings = "afternoon";
+  } else {
+    greetings = "evening";
+  }
 
   return (
     <>
-        <h1>Good {greetings}!</h1>
+      <h1>Good {greetings}!</h1>
     </>
-  )
-}
+  );
+};
 
-export default Greeting
+export default Greeting;
--- admin/src/pages/Home/index.js
+++ admin/src/pages/Home/index.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import HomeForm from "../../components/HomeForm"
+import "./styles.css"
+import Food from "../../assets/food.jpg"
+
 
 const Home = () => {
   return (
     <>
       <div className='container'>
+        <h1>Available Sections</h1>
+        <div className="cards">
+
+          <div className='card'>
+            <img src={Food} alt="" />
+            <div className='action'>
+              <button className='--action'>Edit</button>
+              <button className='--action'>Delete</button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <img src={Food} alt="" />
+            <div className='action'>
+              <button className='--action'>Edit</button>
+              <button className='--action'>Delete</button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <img src={Food} alt="" />
+            <div className='action'>
+              <button className='--action'>Edit</button>
+              <button className='--action'>Delete</button>
+            </div>
+          </div>
+
+          <div className='card'>
+            <img src={Food} alt="" />
+            <div className='action'>
+              <button className='--action'>Edit</button>
+              <button className='--action'>Delete</button>
+            </div>
+          </div>
+        </div>
         <div className='create'>
           <h1>Create New Category</h1>
           <HomeForm />
